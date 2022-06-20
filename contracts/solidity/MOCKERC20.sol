83b25c6c0676b1cf56aad0a5f5bd2f73b8e11052
Paramet Kongjaroen <parametkongjaroen@Prame-2.local>
2022-06-20 12:51:40 +0700

    :triangular_flag_on_post: add feature tokens for imprementing

---
+++ contracts/solidity/MOCKERC20.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+
+    constructor() ERC20("Mock Token", "MTK")  {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+}
