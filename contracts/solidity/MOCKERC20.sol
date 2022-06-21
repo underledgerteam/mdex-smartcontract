64778cf8feec9eda1f8b9eccd729995cbd308e68
Paramet Kongjaroen <parametkongjaroen@Prame-2.local>
2022-06-21 22:57:42 +0700

    update

--- contracts/solidity/MOCKERC20.sol
+++ contracts/solidity/MOCKERC20.sol
@@ -7,8 +7,8 @@ contract MockERC20 is ERC20 {
 
     constructor() ERC20("Mock Token", "MTK")  {}
 
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount);
+    function mint(address to, uint256 amountss) external {
+        _mint(to, amountss);
     }
 
 }
