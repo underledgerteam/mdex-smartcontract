e6891e45041264df71246bb76611eb88b3f5bd7e
littletanggg <wichitaserchuit@gmail.com>
2022-07-05 18:27:44 +0700

    :white_check_mark: Add get virtual price testcase

--- test/curve-pool.js
+++ test/curve-pool.js
@@ -70,127 +70,127 @@ describe("TEST CURVE POOL", () => {
   console.log("Deploy Pool ");
   printSeperator();
 
-  // it("Use Case #1 : Should Add Liquidity", async () => {
-  //   liquidity1 = 10000
-  //   liquidity2 = 20000
+  it("Use Case #1 : Should Add Liquidity", async () => {
+    liquidity1 = 10000
+    liquidity2 = 20000
 
-  //   await token1.mint(user1.address, totalSupply);
-  //   await token2.mint(user1.address, totalSupply);
-  //   await curveToken.connect(deployer).set_minter(pool1.address);
+    await token1.mint(user1.address, totalSupply);
+    await token2.mint(user1.address, totalSupply);
+    await curveToken.connect(deployer).set_minter(pool1.address);
 
-  //   await token1.connect(user1).approve(pool1.address, totalSupply);
-  //   await token2.connect(user1).approve(pool1.address, totalSupply);
+    await token1.connect(user1).approve(pool1.address, totalSupply);
+    await token2.connect(user1).approve(pool1.address, totalSupply);
 
-  //   await expect(
-  //     pool1
-  //       .connect(user1)
-  //       .add_liquidity([liquidity1, liquidity2], 0)
-  //   ).to.emit(curveToken, "Transfer")
-  //     .withArgs(
-  //       "0x0000000000000000000000000000000000000000",
-  //       user1.address,
-  //       (liquidity1 + liquidity2) - 1
-  //     );
-  // });
+    await expect(
+      pool1
+        .connect(user1)
+        .add_liquidity([liquidity1, liquidity2], 0)
+    ).to.emit(curveToken, "Transfer")
+      .withArgs(
+        "0x0000000000000000000000000000000000000000",
+        user1.address,
+        (liquidity1 + liquidity2) - 1
+      );
+  });
 
-  // it("Use Case #2 : Should Add Pool", async () => {
-  //   n_coin = 2
-  //   poolName = "Pool2AB"
+  it("Use Case #2 : Should Add Pool", async () => {
+    n_coin = 2
+    poolName = "Pool2AB"
 
-  //   poolCountBefore = await poolInfo.get_pool_count()
+    poolCountBefore = await poolInfo.get_pool_count()
 
-  //   await registry.add_pool_without_underlying(
-  //     pool1.address, n_coin, curveToken.address, rateInfo,
-  //     decimal, decimal, false, false, poolName
-  //   );
+    await registry.add_pool_without_underlying(
+      pool1.address, n_coin, curveToken.address, rateInfo,
+      decimal, decimal, false, false, poolName
+    );
 
-  //   poolCountAfter = await poolInfo.get_pool_count()
-  //   poolRegistered = await registry.pool_list(0)
+    poolCountAfter = await poolInfo.get_pool_count()
+    poolRegistered = await registry.pool_list(0)
 
-  //   expect(poolCountBefore, 0)
-  //   expect(poolCountAfter, 1)
-  //   expect(poolRegistered, pool1.address)
-  // });
+    expect(poolCountBefore, 0)
+    expect(poolCountAfter, 1)
+    expect(poolRegistered, pool1.address)
+  });
 
-  // it("User Case #3 : Should Get Pool By Address", async () => {
-  //   n_coin = 2
-  //   poolName = "Pool2AB"
+  it("User Case #3 : Should Get Pool By Address", async () => {
+    n_coin = 2
+    poolName = "Pool2AB"
 
-  //   await registry.add_pool_without_underlying(
-  //     pool1.address, n_coin, curveToken.address, rateInfo,
-  //     decimal, decimal, false, false, poolName
-  //   );
+    await registry.add_pool_without_underlying(
+      pool1.address, n_coin, curveToken.address, rateInfo,
+      decimal, decimal, false, false, poolName
+    );
 
-  //   data = await poolInfo.get_pool_by_address(pool1.address)
-  //   count = await poolInfo.get_pool_count()
+    data = await poolInfo.get_pool_by_address(pool1.address)
+    count = await poolInfo.get_pool_count()
 
-  //   expect(count, 1)
-  //   expect(data.name, poolName)
-  //   expect(data.lp_token, curveToken.address)
-  //   expect(data.coins.length, 2)
-  // });
+    expect(count, 1)
+    expect(data.name, poolName)
+    expect(data.lp_token, curveToken.address)
+    expect(data.coins.length, 2)
+  });
 
-  // it("User Case #4 : Should Get Pool List", async () => {
-  //   // Pool 1
-  //   await registry.add_pool_without_underlying(
-  //     pool1.address, 2, curveToken.address, rateInfo,
-  //     decimal, decimal, false, false, "Pool2AB"
-  //   );
+  it("User Case #4 : Should Get Pool List", async () => {
+    // Pool 1
+    await registry.add_pool_without_underlying(
+      pool1.address, 2, curveToken.address, rateInfo,
+      decimal, decimal, false, false, "Pool2AB"
+    );
 
-  //   // Pool 2
-  //   await registry.add_pool_without_underlying(
-  //     pool2.address, 2, curveToken.address, rateInfo,
-  //     decimal, decimal, false, false, "Pool2AC"
-  //   );
+    // Pool 2
+    await registry.add_pool_without_underlying(
+      pool2.address, 2, curveToken.address, rateInfo,
+      decimal, decimal, false, false, "Pool2AC"
+    );
 
-  //   poolList = []
-  //   count = await poolInfo.get_pool_count()
+    poolList = []
+    count = await poolInfo.get_pool_count()
 
-  //   for(i = 0; i <count; i++) {
-  //     poolAddress = await registry.pool_list(i)
-  //     poolData = await poolInfo.get_pool_by_address(poolAddress)
-  //     poolList.push(poolData)
-  //   }
+    for(i = 0; i <count; i++) {
+      poolAddress = await registry.pool_list(i)
+      poolData = await poolInfo.get_pool_by_address(poolAddress)
+      poolList.push(poolData)
+    }
 
-  //   expect(poolList.length, 2)
-  //   expect(poolList[0].address, pool1.address)
-  //   expect(poolList[1].address, pool2.address)
-  // });
+    expect(poolList.length, 2)
+    expect(poolList[0].address, pool1.address)
+    expect(poolList[1].address, pool2.address)
+  });
 
-  // it("Use Case #5 : Should Get Pool Coins", async () => {
-  //   await registry.add_pool_without_underlying(
-  //     pool1.address, 2, curveToken.address, rateInfo,
-  //     decimal, decimal, false, false, "Pool2AB"
-  //   );
+  it("Use Case #5 : Should Get Pool Coins", async () => {
+    await registry.add_pool_without_underlying(
+      pool1.address, 2, curveToken.address, rateInfo,
+      decimal, decimal, false, false, "Pool2AB"
+    );
 
-  //   poolCoins = await poolInfo.get_pool_coins(pool1.address)
+    poolCoins = await poolInfo.get_pool_coins(pool1.address)
     
-  //   expect(poolCoins.length, 2)
-  //   expect(poolCoins, [token1.address, token2.address])
-  // });
+    expect(poolCoins.length, 2)
+    expect(poolCoins, [token1.address, token2.address])
+  });
 
-  // it("Use Case #6 : Should Swap", async () => {
-  //   liquidity1 = 10000
-  //   liquidity2 = 20000
-  //   swapAmount = 10
+  it("Use Case #6 : Should Swap", async () => {
+    liquidity1 = 10000
+    liquidity2 = 20000
+    swapAmount = 10
 
-  //   await token1.mint(user1.address, totalSupply);
-  //   await token2.mint(user1.address, totalSupply);
-  //   await curveToken.connect(deployer).set_minter(pool1.address);
+    await token1.mint(user1.address, totalSupply);
+    await token2.mint(user1.address, totalSupply);
+    await curveToken.connect(deployer).set_minter(pool1.address);
 
-  //   await token1.connect(user1).approve(pool1.address, totalSupply);
-  //   await token2.connect(user1).approve(pool1.address, totalSupply);
+    await token1.connect(user1).approve(pool1.address, totalSupply);
+    await token2.connect(user1).approve(pool1.address, totalSupply);
 
-  //   await pool1.connect(user1).add_liquidity([liquidity1, liquidity1], 0);
+    await pool1.connect(user1).add_liquidity([liquidity1, liquidity1], 0);
 
-  //   min_dy = await pool1.connect(user1).get_dy(0, 1, swapAmount);
+    min_dy = await pool1.connect(user1).get_dy(0, 1, swapAmount);
 
-  //   await expect(
-  //     pool1
-  //       .connect(user1)
-  //       .exchange(0, 1, 10, min_dy, user1.address))
-  //     .to.emit(pool1, "TokenExchange");
-  // });
+    await expect(
+      pool1
+        .connect(user1)
+        .exchange(0, 1, 10, min_dy, user1.address))
+      .to.emit(pool1, "TokenExchange");
+  });
 
   it("Use Case #7 : Get Virtual Price", async () => {
     liquidity1 = 10000
@@ -205,8 +205,7 @@ describe("TEST CURVE POOL", () => {
 
     await pool1.connect(user1).add_liquidity([liquidity1, liquidity2], 0);
 
-    price = await pool1.connect(user1).get_virtual_price();
-    console.log("price", price);
+    await expect(pool1.connect(user1).get_virtual_price()).to.not.equal(0);
   });
 
 });
